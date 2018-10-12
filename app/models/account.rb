edc797e0a77778c371fca5e321a3d8daeb680d91
multiplexd <multi@in-addr.xyz>
2018-10-12 16:40:06 +0100

    Import Interlinked's metadata field increase patch

--- app/models/account.rb
+++ app/models/account.rb
@@ -76,7 +76,7 @@ class Account < ApplicationRecord
   validates_with UnreservedUsernameValidator, if: -> { local? && will_save_change_to_username? }
   validates :display_name, length: { maximum: 30 }, if: -> { local? && will_save_change_to_display_name? }
   validates :note, note_length: { maximum: 160 }, if: -> { local? && will_save_change_to_note? }
-  validates :fields, length: { maximum: 4 }, if: -> { local? && will_save_change_to_fields? }
+  validates :fields, length: { maximum: 16 }, if: -> { local? && will_save_change_to_fields? }
 
   scope :remote, -> { where.not(domain: nil) }
   scope :local, -> { where(domain: nil) }
@@ -258,7 +258,7 @@ class Account < ApplicationRecord
     self[:fields] = fields
   end
 
-  DEFAULT_FIELDS_SIZE = 4
+  DEFAULT_FIELDS_SIZE = 16
 
   def build_fields
     return if fields.size >= DEFAULT_FIELDS_SIZE
